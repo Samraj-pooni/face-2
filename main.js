b04ff5c9181b384916eb6f8644626a8519b8c8e4
Samraj-pooni <87445416+Samraj-pooni@users.noreply.github.com>
2022-01-12 10:49:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format:'png',
+    png_quality:100
+});
+var camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='snapshot' src='"+ data_uri+"'>"
+    });
+}
+
+console.log("ml5 version", ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/train/image/1PN1h_CxF_TijCx8OGheZ1Bl84dbKwq7T',modleLoaded)
